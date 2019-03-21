f98f60b77ff9dd588ad275c3dd5e561394632706
Maxime <25878048+mcsime92@users.noreply.github.com>
2019-03-21 14:55:07 +0100

    Evaluate and Tune the ANN
    Evaluate and Tune the ANN

--- ann_python36.py
+++ ann_python36.py
@@ -4,9 +4,12 @@ from sklearn.preprocessing import LabelEncoder, OneHotEncoder, StandardScaler
 from sklearn.model_selection import train_test_split
 from keras.layers import Dense, Dropout
 from keras.models import Sequential
+from sklearn.metrics import confusion_matrix
+
+DIR = r'C:\Users\A747043\Desktop\My documents\Python\Python36\PyCharm Projects\Deep Learning\ANN'
 
 # Import dataset
-dataset = pd.read_csv("Churn_Modelling.csv")
+dataset = pd.read_csv(DIR + '\Churn_Modelling.csv')
 
 ### Dataset features
 # First 3 are irrelevant 'RowNumber', 'CustomerId', 'Surname'
@@ -77,3 +80,73 @@ classifier.fit(X_train, y_train, batch_size=10, epochs=100)
 # Predicting the test set results
 y_pred = classifier.predict(X_test)
 y_pred = (y_pred > 0.5)
+
+### Predict a single individual
+"""Will this customer leave the bank?
+Geography: Spain
+Credit Score: 650
+Gender: Male
+Age: 55
+Tenure: 2
+Balance: 65000
+Number of Products: 2
+Has Credit Card: Yes
+Is Active Member: Yes
+Estimated Salary: 60000"""
+
+# Use double [[ ]] because we want an horizontal array
+# Geography: 3 countries possible. 0.0, 0.0 is France, 0.0, 1.0 is Spain, 1.0, 0.0 Germany. Other variables are clear.
+new_prediction = classifier.predict(sc.transform(np.array([[0.0, 1.0, 650, 1, 55, 2, 650000, 2, 1, 1, 60000]])))
+new_prediction = (new_prediction > 0.5)
+
+# Confusion Matrix
+cm = confusion_matrix(y_test, y_pred)
+
+# Evaluating the ANN with cross_validation
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import cross_val_score
+def build_classifier():
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier, batch_size = 10, epochs = 100)
+
+# n_jobs = -1 means all CPUs will be used
+accuracies = cross_val_score(estimator = classifier, X = X_train, y = y_train, cv = 10, n_jobs = -1)
+
+# Given the possible variance between results we take the average of the accuracies
+mean = accuracies.mean()
+variance = accuracies.std()
+
+## Results:
+# Mean of accuracies 0.8394999952986837
+# Variance of accuracies 0.018991774234018002
+
+### Tuning the ANN
+from keras.wrappers.scikit_learn import KerasClassifier
+from sklearn.model_selection import GridSearchCV
+from keras.models import Sequential
+from keras.layers import Dense
+def build_classifier(optimizer):
+    classifier = Sequential()
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu', input_dim = 11))
+    classifier.add(Dense(units = 6, kernel_initializer = 'uniform', activation = 'relu'))
+    classifier.add(Dense(units = 1, kernel_initializer = 'uniform', activation = 'sigmoid'))
+    classifier.compile(optimizer = optimizer, loss = 'binary_crossentropy', metrics = ['accuracy'])
+    return classifier
+classifier = KerasClassifier(build_fn = build_classifier)
+parameters = {'batch_size': [25, 32],
+              'epochs': [100, 500],
+              'optimizer': ['adam', 'rmsprop']}
+grid_search = GridSearchCV(estimator = classifier,
+                           param_grid = parameters,
+                           scoring = 'accuracy',
+                           cv = 10)
+grid_search = grid_search.fit(X_train, y_train)
+best_parameters = grid_search.best_params_
+best_accuracy = grid_search.best_score_
+
+# Best parameters turn out to be: batch_size 25, epochs 500, optimizer rmsprop
